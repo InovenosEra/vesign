2a8b595fcd16438336a1c048d37ad7c6edb0d5be
Inovenos <laufer.israel@gmail.com>
2026-02-19 15:23:38 +0200

    Create research training pipeline

---
+++ research/research_main.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from features.forward_returns import compute_forward_returns
+from scoring.weight_training import train_factor_weights
+from backtesting.backtest_engine import run_backtest
+
+
+def run_training():
+    compute_forward_returns()
+    train_factor_weights()
+    run_backtest()
+
+
+if __name__ == "__main__":
+    run_training()
