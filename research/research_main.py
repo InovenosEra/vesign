a1f19bb03538a84a4de158adde02ac9c3d780dae
Inovenos <laufer.israel@gmail.com>
2026-02-19 17:57:03 +0200

    Update research pipeline to use unified backtesting engine

--- research/research_main.py
+++ research/research_main.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from features.forward_returns import compute_forward_returns
 from models.train import train_factor_weights
-from backtesting.backtest_engine import run_backtest
+from backtesting.engine import run_backtest
 
 
 def run_training():
